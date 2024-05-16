c5a95df684ce47cdbfb6f36ef978bd3c73c839ec
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-05-16 21:48:54 +0530

    feat: adding more methods

--- Controllers/GreetingsController.cs
+++ Controllers/GreetingsController.cs
@@ -9,7 +9,17 @@ namespace AspnetCoreJwt.Controllers;
 [ApiController]
 public class GreetingsController : ControllerBase
 {
+    // GET: api/greetings
     [HttpGet]
     public IActionResult Get() => Ok("Hello everyone");
+
+    // GET: api/greetings/public
+    [HttpGet("public")]
+    [AllowAnonymous]
+    public IActionResult PublicMethod() => Ok("Hello everyone");
+
+    // POST: api/greetings
+    [HttpPost]
+    public IActionResult Post() => Ok("Posted successfully");
 
 }
